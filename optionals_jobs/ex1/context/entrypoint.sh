30eb0ed6454ed33027fe5647a0b0cbbe13966c04
fgonzalezr1998 <fergonzaramos@yahoo.es>
2021-02-03 22:44:14 +0100

    [optional job] changes added

--- optionals_jobs/ex1/context/entrypoint.sh
+++ optionals_jobs/ex1/context/entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 mkdir /media/lab
-sshfs fernando@f-l2108-pc02.aulas.etsit.urjc.es:/home/alumnos/fernando /media/lab
+ssh -o StrictHostKeyChecking=no fernando@212.128.254.4
+#sshfs fernando@f-l2108-pc02.aulas.etsit.urjc.es:/home/alumnos/fernando /media/lab
 iperf -s -p 9999 &
 bash
